ec6c7f71567865037ab809e8935fd0b03ed5dfe1
chasonmj <37314758+chasonmj@users.noreply.github.com>
2018-05-05 16:56:59 -0500

    - Added social and links to facebook and twitter

--- app/src/main/java/edu/chason/matt/finalapp/social.java
+++ app/src/main/java/edu/chason/matt/finalapp/social.java
@@ -1,7 +1,10 @@
 package edu.chason.matt.finalapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class social extends AppCompatActivity {
 
@@ -10,4 +13,19 @@ public class social extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social);
     }
+
+    /** Called when the user taps the Facebook button */
+    public void sendMessage(View view) {
+        switch (view.getId()) {
+            case R.id.button2:
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse ("https://touch.facebook.com/" )));
+            break;
+            case R.id.button3:
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse ("https://twitter.com/login?lang=en" )));
+            break;
+        }
+        //startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse ("https://touch.facebook.com/" )));
+    }
+
+
 }
